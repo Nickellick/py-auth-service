ed270d5957f85538f019b67d1cf66b77b4b89e18
Nikolay Povarov <nd.povarov@gmail.com>
2025-07-09 22:04:32 +0300

    fix: changed user creation logic in more appropriate way

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,39 +1,31 @@
-from types import new_class
-
-from fastapi import APIRouter, Depends, HTTPException, status, Request
+from fastapi import APIRouter, Depends, HTTPException, Request, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.dependencies.db import get_db
-from app.models.refresh_token import RefreshToken
 from app.models.user import User
 from app.schemas.user import UserCreate, UserLogin, UserRead
-from app.core.security import get_password_hash
+from app.core.security import create_hash, verify_hash
+from app.services.user import generate_jwt_for_user
 
 router = APIRouter()
 
-@router.post("/login", response_model=UserRead)
-async def login(user_in: UserCreate, db: AsyncSession = Depends(get_db)) -> User:
+@router.post("/login", response_model=UserLogin)
+async def login(user_in: UserCreate, request: Request, db: AsyncSession = Depends(get_db)) -> UserRead:
         result = await db.execute(select(User).where(User.username == user_in.username))
-        if not (user := result.scalar()):
-            raise HTTPException(status_code=404, detail="User not found")
-        return user
+        if not (user := result.scalar()) or not (verify_hash(user_in.password, str(user.password_hash or ''))):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User or password incorrect")
+        return await generate_jwt_for_user(user, request, db)
 
 
 @router.post("/register", response_model=UserRead)
-async def register(user_in: UserLogin, request: Request, db: AsyncSession = Depends(get_db)) -> User:
-    user_agent = request.headers.get("user-agent", "unknown")
-    ip = request.client.host if request.client else "unknown"
+async def register(user_in: UserCreate, request: Request, db: AsyncSession = Depends(get_db)) -> UserRead:
     result = await db.execute(select(User).where(User.username == user_in.username))
     if result.scalar():
-        raise HTTPException(status_code=400, detail="Username already exists")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists")
     user = User(
         username=user_in.username,
         email=user_in.email,
-        password_hash=get_password_hash(user_in.password)
+        password_hash=create_hash(user_in.password)
     )
-    # TODO Create token
-    db.add(user)
-    await db.commit()
-    await db.refresh(user)
-    return user
+    return await generate_jwt_for_user(user, request, db)
