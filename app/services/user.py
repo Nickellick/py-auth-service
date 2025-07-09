54265a2d04d67987b5bf32938d1b040d4a88e041
Nikolay Povarov <nd.povarov@gmail.com>
2025-07-09 22:03:56 +0300

    feat: add pyjwt and move the user's jwt generator to a separate service

---
+++ app/services/user.py
@@ -0,0 +1,49 @@
+import datetime
+
+from fastapi import Request, Depends
+from jwt import encode
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.security import create_hash
+from app.dependencies.db import get_db
+from app.models.refresh_token import RefreshToken
+from app.models.user import User
+from app.schemas.user import UserRead
+from config import config
+
+async def generate_jwt_for_user(user: User, request: Request, db: AsyncSession = Depends(get_db)) -> UserRead:
+    user_agent = request.headers.get("user-agent", "unknown")
+    ip = request.client.host if request.client and request.client.host else "unknown"
+    issued_at = datetime.datetime.now(datetime.UTC)
+    refresh_expires_at = issued_at + datetime.timedelta(days=config.jwt.refresh_token_expire_days)
+    access_expired_at = issued_at + datetime.timedelta(minutes=config.jwt.access_token_expire_minutes)
+    refresh_token = encode({
+        'refresh': True,
+        'uid': user.id,
+        'iat': issued_at.isoformat(),
+        'exp': refresh_expires_at.isoformat(),
+        'roles': user.roles,
+    }, algorithm=config.jwt.algorithm, key=config.jwt.key)
+    access_token = encode({
+        'refresh': False,
+        'uid': user.id,
+        'iat': issued_at.isoformat(),
+        'exp': access_expired_at.isoformat(),
+        'roles': user.roles,
+    }, algorithm=config.jwt.algorithm, key=config.jwt.key)
+    db_token = RefreshToken(
+        user_id=user.id,
+        issued_at=issued_at,
+        expires_at=refresh_expires_at,
+        refresh_token_hash=create_hash(refresh_token),
+        user_agent=user_agent,
+        ip_address=ip
+    )
+    db.add(db_token)
+    user.refresh_tokens.append(db_token)
+    await db.commit()
+    await db.refresh(user)
+    response = UserRead.model_validate(user)
+    response.access_token = access_token
+    response.refresh_token = refresh_token
+    return response
